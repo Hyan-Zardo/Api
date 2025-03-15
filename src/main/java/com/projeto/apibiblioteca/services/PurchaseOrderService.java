27ad2d0a680aa813383e02b6b3a25b340dfeaf21
Hyan Zardo <hyanpeixotoz@gmail.com>
2025-03-14 23:43:52 -0300

    Problema no OrderMapper

--- src/main/java/com/projeto/apibiblioteca/services/PurchaseOrderService.java
+++ src/main/java/com/projeto/apibiblioteca/services/PurchaseOrderService.java
@@ -11,7 +11,7 @@ import org.springframework.stereotype.Service;
 import java.util.UUID;
 
 @Service
-public class OrderService {
+public class PurchaseOrderService {
 
     @Autowired
     private OrderRepository orderRepository;
