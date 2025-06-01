2a597c5b99f2f0cb2e72f40773c812c005bb4505
Hyan-Zardo <hyanpeixotoz@gmail.com>
2025-06-01 11:33:34 -0300

    Home

---
+++ src/main/java/com/projeto/apibiblioteca/controllers/HomeController.java
@@ -0,0 +1,14 @@
+package com.projeto.apibiblioteca.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+    @GetMapping("/")
+    public String home() {
+        return "API está funcionando!";
+    }
+}
+
