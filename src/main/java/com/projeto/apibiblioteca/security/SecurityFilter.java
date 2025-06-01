69834ad4fc891d7adbd6391532fd3e31b9e4f257
Hyan-Zardo <hyanpeixotoz@gmail.com>
2025-06-01 13:26:47 -0300

    Filter certo

--- src/main/java/com/projeto/apibiblioteca/security/SecurityFilter.java
+++ src/main/java/com/projeto/apibiblioteca/security/SecurityFilter.java
@@ -24,17 +24,27 @@ public class SecurityFilter extends OncePerRequestFilter {
     private UserRepository userRepository;
 
     @Override
-    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+            throws ServletException, IOException {
+
+        String path = request.getRequestURI();
+        if (path.equals("/auth/login") || path.equals("/auth/register")) {
+            filterChain.doFilter(request, response);
+            return;
+        }
+
         var token = this.recoverToken(request);
-        if(token != null){
+        if (token != null) {
             var subject = tokenService.validateToken(token);
             UserDetails user = userRepository.findByEmail(subject);
 
             var authentication = new UsernamePasswordAuthenticationToken(user, null, user.getAuthorities());
             SecurityContextHolder.getContext().setAuthentication(authentication);
         }
+
         filterChain.doFilter(request, response);
     }
+
 
     private String recoverToken(HttpServletRequest request){
         var authHeader = request.getHeader("Authorization");
