d379fe345815d2f0d80a8ce94aa5c9775ade23b1
Hyan-Zardo <hyanpeixotoz@gmail.com>
2025-06-01 18:15:39 -0300

    PUTAQUEPARIU

--- src/main/java/com/projeto/apibiblioteca/repositories/BookRepository.java
+++ src/main/java/com/projeto/apibiblioteca/repositories/BookRepository.java
@@ -12,22 +12,22 @@ import java.util.UUID;
 
 @Repository
 public interface BookRepository extends JpaRepository<Book, UUID> {
-//    @Query("""
-//    SELECT b FROM Book b
-//    WHERE (:title IS NULL OR LOWER(b.title) LIKE LOWER(CONCAT('%', :title, '%')))
-//      AND (:isbn IS NULL OR LOWER(b.isbn) LIKE LOWER(CONCAT('%', :isbn, '%')))
-//      AND (:category IS NULL OR LOWER(b.category) LIKE LOWER(CONCAT('%', :category, '%')))
-//      AND (:author IS NULL OR LOWER(b.author) LIKE LOWER(CONCAT('%', :author, '%')))
-//      AND (:conservation IS NULL OR b.conservation = :conservation)
-//      AND (:quantity IS NULL OR b.quantity = :quantity)
-//""")
+    @Query("""
+    SELECT b FROM Book b
+    WHERE (:title IS NULL OR LOWER(b.title) LIKE LOWER(CONCAT('%', :title, '%')))
+      AND (:isbn IS NULL OR LOWER(b.isbn) LIKE LOWER(CONCAT('%', :isbn, '%')))
+      AND (:category IS NULL OR LOWER(b.category) LIKE LOWER(CONCAT('%', :category, '%')))
+      AND (:author IS NULL OR LOWER(b.author) LIKE LOWER(CONCAT('%', :author, '%')))
+      AND (:conservation IS NULL OR b.conservation = :conservation)
+      AND (:quantity IS NULL OR b.quantity = :quantity)
+""")
     List<Book> findByFilters(
             @Param("title") String title,
-            @Param("isbn") String author,
-            @Param("category") String publisher,
-            @Param("author") String category,
+            @Param("isbn") String isbn,
+            @Param("category") String category,
+            @Param("author") String author,
             @Param("conservation") BookConservation conservation,
-            @Param("quantity") Integer edition
+            @Param("quantity") Integer quantity
     );
 
 }
