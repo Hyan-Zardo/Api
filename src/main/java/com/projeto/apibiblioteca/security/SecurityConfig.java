136bd078de5ce178b415d1cb46d00272f4f9d7c2
Hyan-Zardo <hyanpeixotoz@gmail.com>
2025-06-03 10:11:28 -0300

    Permitindo frontend

--- src/main/java/com/projeto/apibiblioteca/security/SecurityConfig.java
+++ src/main/java/com/projeto/apibiblioteca/security/SecurityConfig.java
@@ -44,6 +44,7 @@ public class SecurityConfig {
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
         config.setAllowedOrigins(List.of("http://localhost:5173"));
+        config.setAllowedOrigins(List.of("valley-library.vercel.app"));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE"));
         config.setAllowedHeaders(List.of("*"));
         config.setAllowCredentials(true);
