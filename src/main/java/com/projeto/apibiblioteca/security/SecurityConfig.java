6850707812bdb1f785ca2b52a162eafed995736f
Hyan-Zardo <hyanpeixotoz@gmail.com>
2025-06-03 10:46:53 -0300

    .....

--- src/main/java/com/projeto/apibiblioteca/security/SecurityConfig.java
+++ src/main/java/com/projeto/apibiblioteca/security/SecurityConfig.java
@@ -43,8 +43,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
-        config.setAllowedOrigins(List.of("http://localhost:5173"));
-        config.setAllowedOrigins(List.of("valley-library.vercel.app"));
+        config.setAllowedOrigins(List.of("http://localhost:5173", "https://valley-library.vercel.app"));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE"));
         config.setAllowedHeaders(List.of("*"));
         config.setAllowCredentials(true);
